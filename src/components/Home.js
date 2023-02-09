497496136ab5165b4663decbc8979d75420300b6
y-komurasaki <yuusuke3tamaria19880415@gmail.com>
2023-02-09 19:29:57 +0900

    記事投稿データの取得、表示

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,42 @@
-import React from 'react';
+import { getDocs, collection } from "firebase/firestore";
+import React, { useEffect, useState } from 'react';
+import { db } from "../firebase";
 import "./Home.css";
 
 const Home = () => {
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+     const getPosts = async () => {
+      const data = await getDocs(collection(db, "posts"));
+      // console.log(data);
+      // console.log(data.docs);
+      // console.log(data.docs.map((doc) => ({ doc })));
+      //console.log(data.docs.map((doc) => ({...doc.data(), id: doc.id })));
+      setPostList(data.docs.map((doc) => ({...doc.data(), id: doc.id })));
+     };
+     getPosts();
+  },[]);
+
   return (
     <div className='homePage'>
-      <div className='postContents'>
-          <div className='postHeader'>
-            <h1>タイトル</h1>
-          </div>
+      {postList.map((post) => {
+        return (
+          <div className='postContents' key={post.id}>
+            <div className='postHeader'>
+              <h1>{post.title}</h1>
+            </div>
         
-        <div className='postTextContainer'>
-          今はReactの学習中です。これからが頑張ってReactエンジニアとして活躍していきたいです。よろしくお願いします。
+            <div className='postTextContainer'>{post.postText}</div>
+            <div className='nameAndDeleteButton'>
+              <h3>@{post.author ? post.author.username : "未登録のユーザー"}</h3>
+              <button>削除</button>
+            </div>
         </div>
-        <div className='nameAndDeleteButton'>
-          <h3>dot_kom</h3>
-          <button>削除</button>
-        </div>
-      </div>
+        );
+      })}
     </div>
-  )
+  );
 };
 
 export default Home;
