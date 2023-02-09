361ec095fbe01bc2b6fa87445b863e2098bf0c54
y-komurasaki <yuusuke3tamaria19880415@gmail.com>
2023-02-09 16:23:34 +0900

    Homeコンポーネントデザイン作成

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='homePage'>
+      <div className='postContents'>
+          <div className='postHeader'>
+            <h1>タイトル</h1>
+          </div>
+        
+        <div className='postTextContainer'>
+          今はReactの学習中です。これからが頑張ってReactエンジニアとして活躍していきたいです。よろしくお願いします。
+        </div>
+        <div className='nameAndDeleteButton'>
+          <h3>dot_kom</h3>
+          <button>削除</button>
+        </div>
+      </div>
+    </div>
   )
 };
 
